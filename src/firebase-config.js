991e96ede3bbe5f392c6bf0072afb9b619b2e6f1
Tribhuwan <mathincosmos@gmail.com>
2023-01-21 22:12:48 +0530

    Add sign-in page

---
+++ src/firebase-config.js
@@ -0,0 +1,15 @@
+import {initializeApp } from 'firebase/app'
+// initialize firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyAe7IFhbdssQuk19d0w8efDDqbuN30vOTk",
+    authDomain: "todoer-tjsm.firebaseapp.com",
+    projectId: "todoer-tjsm",
+    storageBucket: "todoer-tjsm.appspot.com",
+    messagingSenderId: "745140377276",
+    appId: "1:745140377276:web:84c3bb1399bd6bb41fa2b9"
+  };
+
+
+
+
+const app = initializeApp(firebaseConfig);
--- src/index.js
+++ src/index.js
@@ -1,20 +1,13 @@
 /* Imports css */
+const fm= document.querySelector(".full-main");
+fm.classList.add('grid');
+// fm.classList.remove("hidden")
+
 import "./style.css";
 import { renderTask, deleteAllProjects, allProjectArr, currentPageName, getProjectByName } from "./domManipulation.js";
 
-// import firebase
-import {initializeApp } from 'firebase/app'
-// initialize firebase
-const firebaseConfig = {
-    apiKey: "AIzaSyAe7IFhbdssQuk19d0w8efDDqbuN30vOTk",
-    authDomain: "todoer-tjsm.firebaseapp.com",
-    projectId: "todoer-tjsm",
-    storageBucket: "todoer-tjsm.appspot.com",
-    messagingSenderId: "745140377276",
-    appId: "1:745140377276:web:84c3bb1399bd6bb41fa2b9"
-  };
 
-const app = initializeApp(firebaseConfig);
+
 
 // import classes
 import { Task } from './task'
